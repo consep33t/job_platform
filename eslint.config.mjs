ecbd4ab6bd6d73f0ea591f4d042ced3f2df93260
Consep33t <agengp360@gmail.com>
2024-12-14 01:35:12 +0700

    fix bug eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,19 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+let eslintConfig;
+try {
+  eslintConfig = [...compat.extends("next/core-web-vitals")];
+} catch (error) {
+  console.error("Error loading ESLint config:", error);
+  eslintConfig = [];
+}
 
-console.log(eslintConfig);
+eslintConfig = eslintConfig.map((config) => {
+  if (config.parser && typeof config.parser.parse === "function") {
+    delete config.parser.parse;
+  }
+  return config;
+});
 
 export default eslintConfig;
